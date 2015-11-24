ec94f3af2db58d6523adccea131f4f5657c6217f
Stefano J. Attardi <stefano@storehouse.co>
2015-11-23 16:58:05 -0800

    Add Segment tracking

--- js/Tracking.js
+++ js/Tracking.js
@@ -9,5 +9,12 @@ Tracking.init = function() {
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
   ga('create', 'UA-1809956-1', 'attardi.org');
   ga('send', 'pageview');
+
+
+  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.1.0";
+  analytics.load("lvbSzlHAjrMBGkpEsCT5zwZiiPShkxWw");
+  analytics.page()
+  }}();
   /* eslint-enable */
+
 };
