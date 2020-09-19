be233e1d9942db7f11b3d1457582ff70ec418d56
Stefano J. Attardi <stefano@attardi.org>
2020-09-19 10:21:56 -0700

    Viewport meta tag

--- jamesandstefano/template.tsx
+++ jamesandstefano/template.tsx
@@ -25,6 +25,10 @@ export const Template = ({
         rel="stylesheet"
       />
       <style dangerouslySetInnerHTML={{ __html: css }} />
+      <meta
+        name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"
+      />
       <link rel="apple-touch-icon" href="/savethedate/images/7.png" />
       <link rel="icon" href="/savethedate/images/7.png" />
     </head>
