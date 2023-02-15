20f0efb654f732489cddfef0b7168dacad16a52f
Stefano J. Attardi <stefano@attardi.org>
2023-02-15 16:15:27 -0500

    RSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,67 @@
 module.exports = {
   siteMetadata: {
     title: 'Stefano J. Attardi',
+    siteUrl: `https://attardi.org/`,
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    // 'gatsby-plugin-react-next',
     'gatsby-plugin-typescript',
     'gatsby-plugin-glamor',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize: ({query: {site, allMarkdownRemark}}) => {
+              return allMarkdownRemark.nodes.map((node) => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{'content:encoded': node.html}],
+                });
+              });
+            },
+            query: `
+            {
+              allMarkdownRemark(
+                sort: { order: DESC, fields: [frontmatter___date] },
+              ) {
+                nodes {
+                  excerpt
+                  html
+                  fields { 
+                    slug 
+                  }
+                  frontmatter {
+                    title
+                    date
+                  }
+                }
+              }
+            }
+          `,
+            output: '/rss.xml',
+            title: 'Stefano J. Attardi',
+          },
+        ],
+      },
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
