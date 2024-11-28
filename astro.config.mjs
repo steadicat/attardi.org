58455b42c9278462e28e1217837e4ead2b761f7c
Stefano J. Attardi <stefano@attardi.org>
2024-11-28 09:16:34 -0500

    Astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,44 +2,28 @@ import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import svelte from '@astrojs/svelte';
 import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
-
-import rehypePrettyCode from 'rehype-pretty-code';
-
-const prettyCodeOptions = {
-  keepBackground: false,
-  theme: JSON.parse(fs.readFileSync('./moonlight-ii.json', 'utf-8')),
-  onVisitLine(node) {
-    if (node.children.length === 0) {
-      node.children = [{type: 'text', value: ' '}];
-    }
-  },
-  onVisitHighlightedLine(node) {
-    if (!node.properties.className) {
-      node.properties.className = [];
-    }
-    node.properties.className.push('highlighted');
-  },
-  onVisitHighlightedWord(node) {
-    node.properties.className = ['word'];
-  },
-  tokensMap: {},
-};
+import expressiveCode from 'astro-expressive-code';
 
 export default defineConfig({
   site: 'https://attardi.org',
   scopedStyleStrategy: 'where',
   integrations: [
-    mdx({
-      // syntaxHighlight: 'prism',
+    expressiveCode({
+      frames: {
+        showCopyToClipboardButton: false,
+      },
+      themes: ['min-dark', 'min-light'],
+      styleOverrides: {
+        codeFontFamily: 'var(--mono)',
+        codeFontSize: '11px',
+        codeFontWeight: '400',
+        codeLineHeight: '18px',
+        codePaddingBlock: '18px',
+        codePaddingInline: '18px',
+      },
     }),
+    mdx({}),
     sitemap(),
     svelte(),
   ],
-  markdown: {
-    extendDefaultPlugins: true,
-    syntaxHighlight: false,
-    rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
-  },
 });
