37b30a32e8ec54ef77bf31676b5e07ce34da386f
Stefano J. Attardi <stefano@attardi.org>
2024-12-06 16:14:12 -0500

    Update Bluesky handle

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -6,7 +6,7 @@ export async function GET(context: {site: any}) {
   return rss({
     title: 'Stefano J. Attardi',
     description:
-      'Engineering Manager at Coinbase. Former UI engineer and designer, specializing in React and React performance. Previously at Facebook and Storehouse. Winner of the first Node.js Knockout with Swarmation.com.',
+      'Sr. Engineering Manager, Developer Experience at Coinbase. Former UI engineer and designer, specializing in React and React performance. Previously at Facebook and Storehouse. Winner of the first Node.js Knockout with Swarmation.com.',
     site: context.site,
     items: articles.map(({slug, data: {title, date, description}}) => ({
       title,
